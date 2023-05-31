308aceb909bd3e757a9bcbed3ca7ba973f6d1e50
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-31 19:28:12 +0400

    added destination page

---
+++ starter-code/script.js
@@ -0,0 +1,35 @@
+$("#dest-menu > li > a").on("click", function() {
+    switch ($(this).attr("id")) {
+        case "moon_btn":
+            changeOver("Moon")
+            break;
+        case "mars_btn":
+            changeOver("Mars")
+            break;
+        case "europa_btn":
+            changeOver("Europa")
+            break;
+        case "titan_btn":
+            changeOver("Titan")
+            break;
+        default:
+            console.log("Something went wrong");
+    }
+})
+
+
+function changeOver(planet) {
+    $.ajax({
+      url: "./data.json",
+      dataType: 'json',
+      success: function(jsonData) {
+          var i = jsonData.destinations.filter(x => x.name === planet);
+          $("#planet_image").attr("src", i[0].images.png)
+          $("#planet_name").text(i[0].name);
+          $("#description").text(i[0].description)
+          $("#distance").text(i[0].distance)
+          $("#travel_time").text(i[0].travel)
+          console.log(i, planet, jsonData);
+      }
+    });
+}
